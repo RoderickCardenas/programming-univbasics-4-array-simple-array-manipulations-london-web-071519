a757619fcef04466a496bc89341e434ece52f189
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-06-27 08:57:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def delete_at(array, integer)
-  integer = integer.to_i
+def using_delete_at(array, integer)
   array.delete_at(integer)
 end
