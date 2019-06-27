1215942859c0703c3c9f9cb411c039d5a104abcd
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-06-27 08:37:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,43 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args
+  
+end
+
+def shift_with_args
+  
+end
+
+def using_concat(array1, array2)
+
+end
+
+def using_insert(array, element)
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def delete_at(array, integer)
+  
+end
